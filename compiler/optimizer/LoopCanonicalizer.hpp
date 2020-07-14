f975b2b1e787717243fc89d154f4f11236180ab0
Abdulrahman Alattas <rmnattas@gmail.com>
2020-07-14 12:47:24 -0400

    Request another IVA run after loopCanonicalization when needed
    Signed-off-by: Abdulrahman Alattas <rmnattas@gmail.com>

--- compiler/optimizer/LoopCanonicalizer.hpp
+++ compiler/optimizer/LoopCanonicalizer.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corp. and others
+ * Copyright (c) 2000, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -188,6 +188,7 @@ class TR_LoopTransformer : public TR::Optimization
    TR::TreeTop    *_startOfHeader;
    TR::CFG        *_cfg;
    TR_Structure  *_rootStructure;
+   bool           _structureChanged;
 
    List<TR::Block> _invariantBlocks;
    List<TR::Block> _blocksToBeCleansed;
