b0d33151a6d67674449e4509373de817f3e9451b
Shubham Verma <shubhamv.sv@gmail.com>
2023-04-14 16:48:47 -0400

    fixup! Disable opts for vmfarm builds
    Disable opts only when off heap allocation is enabled

--- compiler/control/OMROptions.cpp
+++ compiler/control/OMROptions.cpp
@@ -2595,13 +2595,16 @@ OMR::Options::jitPreProcess()
    _disabledOptimizations[IVTypeTransformation] = true;
    _disabledOptimizations[basicBlockHoisting] = true;
 
-   // sverma: disable opts known to be broken for off heap
-   _disabledOptimizations[escapeAnalysis] = true;
-   _disabledOptimizations[idiomRecognition] = true;
-   // Disable OSR
-   self()->setOption(TR_DisableOSR);
-   self()->setOption(TR_EnableOSR, false);
-   self()->setOption(TR_EnableOSROnGuardFailure, false);
+   if (TR::Compiler->om.isOffHeapAllocationEnabled())
+      {
+      // sverma: disable opts known to be broken for off heap
+      _disabledOptimizations[escapeAnalysis] = true;
+      _disabledOptimizations[idiomRecognition] = true;
+      // Disable OSR
+      self()->setOption(TR_DisableOSR);
+      self()->setOption(TR_EnableOSR, false);
+      self()->setOption(TR_EnableOSROnGuardFailure, false);
+      }
 
    self()->setOption(TR_DisableTreePatternMatching);
    self()->setOption(TR_DisableHalfSlotSpills);
