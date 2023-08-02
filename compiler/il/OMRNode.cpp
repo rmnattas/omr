57ff0f5637caa1bd87720a39a7f46688a8961670
Shubham Verma <shubhamv.sv@gmail.com>
2023-08-02 17:37:55 -0400

    Add pinning array pointer support in node extension
    Use node extensions to store pinning array pointer for non add nodes.
    
    Signed-off-by: Shubham Verma <shubhamv.sv@gmail.com>

--- compiler/il/OMRNode.cpp
+++ compiler/il/OMRNode.cpp
@@ -215,7 +215,7 @@ OMR::Node::Node(TR::Node *originatingByteCodeNode, TR::ILOpCodes op, uint16_t nu
        + self()->hasBranchDestinationNode()
        + self()->hasBlock()
        + self()->hasArrayStride()
-       + self()->hasPinningArrayPointer()
+       + (self()->hasPinningArrayPointer() && !self()->hasPinningArrayPointerInNodeExtension())
        + self()->hasDataType() <= 1,
          "_unionPropertyA union is not disjoint for this node %s (%p):\n"
          "  has({SymbolReference, ...}, ..., DataType) = ({%1d,%1d},%1d,%1d,%1d,%1d,%1d)\n",
@@ -225,7 +225,7 @@ OMR::Node::Node(TR::Node *originatingByteCodeNode, TR::ILOpCodes op, uint16_t nu
          self()->hasBranchDestinationNode(),
          self()->hasBlock(),
          self()->hasArrayStride(),
-         self()->hasPinningArrayPointer(),
+         (self()->hasPinningArrayPointer() && !self()->hasPinningArrayPointerInNodeExtension()),
          self()->hasDataType());
    }
 
@@ -5061,9 +5061,16 @@ OMR::Node::hasArrayStride()
    }
 
 bool
+OMR::Node::hasPinningArrayPointerInNodeExtension()
+   {
+   int extensionElemNum = _unionBase._extension.getNumElems();
+   return (extensionElemNum >= 6 && self()->getPinningArrayPointer() != NULL);
+   }
+
+bool
 OMR::Node::hasPinningArrayPointer()
    {
-   return self()->getOpCode().hasPinningArrayPointer();
+   return self()->getOpCode().hasPinningArrayPointer() || self()->hasPinningArrayPointerInNodeExtension();
    }
 
 bool
@@ -5203,16 +5210,28 @@ OMR::Node::setArrayStride(int32_t s)
 TR::AutomaticSymbol*
 OMR::Node::getPinningArrayPointer()
    {
-   TR_ASSERT(self()->hasPinningArrayPointer(), "attempting to access _pinningArrayPointer field for node %s %p that does not have it", self()->getOpCode().getName(), this);
-   return _unionPropertyA._pinningArrayPointer;
+   TR_ASSERT(self()->hasPinningArrayPointer() || _unionBase._extension.getNumElems() >= 6, "attempting to access _pinningArrayPointer field for node %s %p that does not support it", self()->getOpCode().getName(), this);
+
+   if (self()->getOpCode().hasPinningArrayPointer())
+      return _unionPropertyA._pinningArrayPointer;
+
+   return _unionBase._extension.getExtensionPtr()->getElem<TR::AutomaticSymbol *>(5);
    }
 
 TR::AutomaticSymbol*
 OMR::Node::setPinningArrayPointer(TR::AutomaticSymbol *s)
    {
    s->setPinningArrayPointer();
-   TR_ASSERT(self()->hasPinningArrayPointer(), "attempting to access _pinningArrayPointer field for node %s %p that does not have it", self()->getOpCode().getName(), this);
-   return (_unionPropertyA._pinningArrayPointer = s);
+   TR_ASSERT(self()->hasPinningArrayPointer(), "attempting to access _pinningArrayPointer field for node %s %p that does not support it", self()->getOpCode().getName(), this);
+
+   if (self()->getOpCode().hasPinningArrayPointer())
+      return _unionPropertyA._pinningArrayPointer = s;
+
+   int extensionElemNum = _unionBase._extension.getNumElems();
+   if (extensionElemNum < 6)
+      self()->addExtensionElements(6 - extensionElemNum);
+
+   return _unionBase._extension.getExtensionPtr()->setElem<TR::AutomaticSymbol *>(5, s);
    }
 
 TR::DataType
