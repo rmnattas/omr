c1a5b8a3a8d02ac0625a3d348cad44219cf210dc
Abdulrahman Alattas <rmnattas@gmail.com>
2026-05-25 13:27:46 -0400

    Slow indecator

--- thread/common/threaddef.h
+++ thread/common/threaddef.h
@@ -485,7 +485,7 @@ enum {
 		char * monitorName = omrthread_monitor_get_name(monitor); \
 		if (getenv("AA_TraceTFMon3")){ \
 			if (monitorName && strncmp(monitorName,"Thread public flags mutex",strlen("Thread public flags mutex"))==0){ \
-				fprintf(stderr, "AA_TraceTFMon3: Acquire monitor 0x%p by thread 0x%p tid=0x%x\n", monitor, self, (int)self->tid); \
+				fprintf(stderr, "AA_TraceTFMon3%s: Acquire monitor 0x%p by thread 0x%p tid=0x%x\n", isSlowEnter?"S":"", monitor, self, (int)self->tid); \
 			} \
 		} \
 	} while (0)
